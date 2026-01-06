84f1c993523037c2078a9d47aaac5fe5f59fee03
Ástríður Haraldsdóttir Passauer <astridur2001@gmail.com>
2026-01-06 09:49:02 +0100

    init cookiecutter project

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for new_project_mlops
---
+++ src/new_project_mlops/train.py
@@ -0,0 +1,10 @@
+from new_project_mlops.model import Model
+from new_project_mlops.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
